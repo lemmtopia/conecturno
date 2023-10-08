08206f7469f922ddc7b2c87a96ea13e50bd557b7
Gabriel G. Cunha <gabrielmesmo97@gmail.com>
2023-10-08 18:37:54 -0300

    Add vestibulares.html and entrevistas.html

--- menu.js
+++ menu.js
@@ -1,5 +1,10 @@
 const menu = document.getElementById("menu");
-const menuContent = '<a href="./index.html" id="menu-link">Sobre nós</a><br><a href="./acontece.html" id="menu-link">Acontece no Noturno</a><br><a href="./vestibulares.html" id="menu-link">Vestibulares</a>';
+const menuContent = '<a href="./index.html" id="menu-link">Sobre nós</a><br><a href="./acontece.html" id="menu-link">Acontece no Noturno</a><br><a href="./vestibulares.html" id="menu-link">Vestibulares</a><br><a href="./entrevistas.html" id="menu-link">Entrevistas</a>';
+
+const logo = document.getElementById("logo");
+logo.onclick = () => {
+    window.location.href = "./index.html";
+}
 
 function showMenu() {
     if (menu.innerHTML == '') menu.innerHTML = menuContent;
